3da4d249d5ad6324dbc1a4c08d387abb3f303837
simoneluca00 <si.zaino@libero.it>
2022-04-01 18:03:25 +0200

    Milestone 3

--- asset/js/main.js
+++ asset/js/main.js
@@ -3,20 +3,11 @@ var app = new Vue(
         el: '#root',
 
         data: {
-            todos: [
-                {
-                    text:'wiskila',
-                    done: false,
-                },
-                {
-                    text:'nto wiskiske',
-                    done: true,
-                },
-                {
-                    text:'allelora',
-                    done: false,
-                },
-            ]
+            
+            todos: [],
+
+            newTask: '',
+            id: 0,
         },
 
         methods: {
@@ -29,9 +20,27 @@ var app = new Vue(
                 }
             },
 
+            // funzione per aggiungere un nuovo elemento alla lista
+            addItem: function(){
+
+                // condizione per non far aggiungere stringhe vuote
+                if (this.newTask.trim() != '') {
+                    
+                    this.todos.push({
+                        text: this.newTask,
+                        done: false,
+                    });
+                    // console.log(this.todos)    
+                }
+                
+                // ogni volta che si aggiunge un elemento il campo input si resetta
+                this.newTask = '';
+            },
+
+            // funzione per rimuovere un elemento dalla lista
             removeItem: function(toDoIndex){
                 this.todos.splice(toDoIndex,1);
-            }
+            },
         }
     }
 )
