e71a789c0765d89ef3051e3e4efd5efc6456aa04
simoneluca00 <si.zaino@libero.it>
2022-04-01 16:47:00 +0200

    Milestone 2

--- asset/js/main.js
+++ asset/js/main.js
@@ -3,11 +3,35 @@ var app = new Vue(
         el: '#root',
 
         data: {
-
+            todos: [
+                {
+                    text:'wiskila',
+                    done: false,
+                },
+                {
+                    text:'nto wiskiske',
+                    done: true,
+                },
+                {
+                    text:'allelora',
+                    done: false,
+                },
+            ]
         },
 
         methods: {
+            // funzione per modificare al click il valore di DONE (toggle non funziona?)
+            doneToDo: function(element) {
+                if( element.done == true ){
+                    element.done = false;
+                } else {
+                    element.done = true;
+                }
+            },
 
+            removeItem: function(toDoIndex){
+                this.todos.splice(toDoIndex,1);
+            }
         }
     }
 )
